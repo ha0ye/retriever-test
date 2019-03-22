5d74dd0b156b64046fa5bbd1eb93dce554bd4d83
Hao Ye <lhopitalified@gmail.com>
2019-03-21 21:57:46 -0400

    initial commit of files

---
+++ main.R
@@ -0,0 +1,4 @@
+reticulate::py_module_available("retriever")
+r_data_retriever <- reticulate::import("retriever")
+r_data_retriever$datasets()
+r_data_retriever$install_csv("ngreatplains-flowering-dates", "{db}_{table}.csv", FALSE, TRUE)
