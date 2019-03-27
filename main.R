ffc11f8fb06f29da27cf801f17ca3e06d492f5c5
Hao Ye <lhopitalified@gmail.com>
2019-03-27 18:29:54 -0400

    remove checks for retriever

--- main.R
+++ main.R
@@ -1,8 +1,8 @@
-
-reticulate::py_module_available("retriever")
-
-r_data_retriever <- reticulate::import("retriever")
-
-r_data_retriever$datasets()
-
-r_data_retriever$install_csv("ngreatplains-flowering-dates", "{db}_{table}.csv", FALSE, TRUE)
+# 
+# reticulate::py_module_available("retriever")
+# 
+# r_data_retriever <- reticulate::import("retriever")
+# 
+# r_data_retriever$datasets()
+# 
+# # r_data_retriever$install_csv("ngreatplains-flowering-dates", "{db}_{table}.csv", FALSE, TRUE)
