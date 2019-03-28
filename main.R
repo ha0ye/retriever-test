0362baa218aba24f116f23bf33fdd8e1098d7385
Hao Ye <lhopitalified@gmail.com>
2019-03-27 22:16:39 -0400

    test reticulate

--- main.R
+++ main.R
@@ -1,8 +1,12 @@
-# 
-# reticulate::py_module_available("retriever")
-# 
-# r_data_retriever <- reticulate::import("retriever")
-# 
-# r_data_retriever$datasets()
-# 
-# # r_data_retriever$install_csv("ngreatplains-flowering-dates", "{db}_{table}.csv", FALSE, TRUE)
+
+reticulate::py_module_available("retriever")
+
+r_data_retriever <- reticulate::import("retriever")
+
+datasets <- r_data_retriever$datasets()
+
+message(lapply(datasets, as.character))
+
+r_data_retriever$install_csv("veg-plots-sdl", data_dir = ".")
+
+message(paste(list.files(pattern = "^veg_plots_sdl_"), collapse = "\n"))
