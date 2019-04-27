73ac7d9beca0f675131cb44a76788212537f85a5
Hao Ye <lhopitalified@gmail.com>
2019-04-27 02:19:24 -0400

    try self-contained test and travis config

--- main.R
+++ main.R
@@ -1,15 +1,19 @@
 library(reticulate)
 use_python("/usr/bin/python3")
 
-stopifnot(reticulate::py_module_available("retriever"))
+# check for retriever availability
+stopifnot(py_module_available("retriever"))
+r_data_retriever <- import("retriever")
 
-r_data_retriever <- reticulate::import("retriever")
-
+# check for dataset availability
 datasets <- r_data_retriever$dataset_names()
-
 found <- grep("veg-plots-sdl", datasets, value = TRUE)
 stopifnot(length(found) > 0)
 
+# download files
 r_data_retriever$install_csv("veg-plots-sdl", data_dir = ".")
 
-message(paste(list.files(pattern = "^veg_plots_sdl_"), collapse = "\n"))
+# check for downloaded files
+data_files <- list.files(pattern = "^veg_plots_sdl_")
+data_folder <- list.files(pattern = "^veg-plots-sdl")
+stopifnot(length(data_files) + length(data_folder) > 0)
